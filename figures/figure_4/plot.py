758c7bc4f06c657abe578bba87183168f7507666
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-12-02 13:18:26 +0100

    solved it

--- figures/figure_4/plot.py
+++ figures/figure_4/plot.py
@@ -171,7 +171,13 @@ def plot_snapshot(fig, n_file,
     ax.set_axis_off()
     ax.set_aspect('equal')
     ax.grid(False)  # <-- disables ProPlot's auto-enabled grid
-
+    # setting the axes limits here is necessary because some methods will be called before plot_2d_axes, and these methods will need the axes limits to be properly set to place, for example, text labels
+    gr.set_2d_axes_limits(ax,
+                          [X_min_max[0][0], X_min_max[1][0]],
+                          [X_min_max[0][1] - X_min_max[0][0],
+                              X_min_max[1][1] - X_min_max[1][0]],
+                          axis_origin=parameters['axis_origin']
+                          )
     # compute the vector field u and store it in U_x, U_y and its related coordinates X_u, Y_u in the current configuration
     U_x = []
     U_y = []
@@ -208,7 +214,7 @@ def plot_snapshot(fig, n_file,
                             legend_arrow_length=0.15,
                             legend_text_arrow_space=0.2,
                             legend_head_over_shaft_length=parameters['legend_head_over_shaft_length'],
-                            legend_position=[0.5, 0.5],
+                            legend_position=[-0.525, 0.71],
                             z_order=1)
 
     # plot X_curr
