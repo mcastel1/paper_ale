04386c0533511dcd9d7da38bcd36edf60a8bb738
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-01-06 18:38:52 +0100

    plotting theta...

--- figures/figure_8/plot.py
+++ figures/figure_8/plot.py
@@ -37,28 +37,28 @@ plt.rcParams.update({
 parameters = io.read_parameters_from_csv_file(
     os.path.join(os.path.dirname(os.path.abspath(__file__)), "parameters.csv")
 )
-
-z_min = -0.5
-z_max = 0
+solution_parameters = io.read_parameters_from_csv_file(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                 "solution_parameters.csv")
+)
 
 
 # define the folder where to read the data
 print("Current working directory:", os.getcwd())
 print("Script location:", os.path.dirname(os.path.abspath(__file__)))
 solution_path = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "solution/nodal_values/"
-)
-solution_ode_path = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "solution-ode"
+    os.path.dirname(os.path.abspath(__file__)), "solution/"
 )
 
-mesh_path = os.path.join(os.path.dirname(
-    os.path.abspath(__file__)), "mesh/solution/")
 figure_path = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), parameters['figure_name'])
 
 
-data_z_ode = pd.read_csv(os.path.join(solution_ode_path, "z_ode.csv"))
+data_theta = pd.read_csv(os.path.join(solution_path, "theta_omega.csv"))
+
+# add step number column
+data_theta['t'] = (data_theta.index + 1) * \
+    solution_parameters['print_out_stride']/solution_parameters['T']
 
 
 fig = pplt.figure(figsize=np.array(parameters['figure_size']),
@@ -70,51 +70,57 @@ fig = pplt.figure(figsize=np.array(parameters['figure_size']),
                   hspace=parameters['hspace'])
 
 # create axes
-# 2d axes
 fig.add_subplot(1, 1, 1)
 
 
-def plot_snapshot(fig, azimuth, altitude):
-
-    ################## 2d plots ################
+def plot(fig):
 
     # =============
-    # z_r subplot
+    # theta plot
     # =============
 
     ax = fig.axes[0]  # Use the existing axis
     ax.set_axis_off()
 
-    data_z_ode["r"] = np.sqrt(
-        data_z_ode[clab.label_x_column] ** 2 + data_z_ode[clab.label_y_column] ** 2)
+    # data_theta["r"] = np.sqrt(
+    #     data_theta[clab.label_x_column] ** 2 + data_theta[clab.label_y_column] ** 2)
 
     # Sort data by r for a proper plot
-    data_z_ode_sorted = data_z_ode.sort_values(by="r")
+    # data_z_ode_sorted = data_theta.sort_values(by="r")
 
-    # Plot z vs r
-    ax.plot(data_z_ode_sorted["r"], data_z_ode_sorted[clab.label_z_column], linestyle="-", linewidth=2, color="red",
+    # Plot theta vs t
+    ax.plot(data_theta['t'], data_theta['theta'],
+            linestyle="-",
+            linewidth=2,
+            color="red",
             zorder=1,
-            label=r' ')
+            label=r' '
+            )
 
-    gr.plot_2d_axes(ax, [0, z_min], [parameters['R'], z_max - z_min],
-                    margin=parameters['axis_margin_2d'],
-                    tick_length=parameters['tick_length_2d'],
-                    line_width=parameters['axis_line_width_2d'],
-                    axis_label=parameters['axis_label_2d_z'],
-                    axis_origin=parameters['axis_origin_2d'],
-                    axis_label_offset=parameters['axis_label_offset_2d'],
-                    tick_label_format=parameters['tick_label_format_2d'],
+    t_min = np.min(data_theta['t'])
+    t_max = np.max(data_theta['t'])
+
+    theta_min = np.min(data_theta['theta'])
+    theta_max = np.max(data_theta['theta'])
+
+    gr.plot_2d_axes(ax, [t_min, theta_min], [t_max - t_min, theta_max-theta_min],
+                    margin=parameters['axis_margin'],
+                    tick_length=parameters['tick_length'],
+                    line_width=parameters['axis_line_width'],
+                    axis_label=parameters['axis_label'],
+                    axis_origin=parameters['axis_origin'],
+                    axis_label_offset=parameters['axis_label_offset'],
+                    tick_label_format=parameters['tick_label_format'],
                     plot_label_font_size=parameters['plot_label_font_size'],
-                    font_size=parameters['font_size_2d'],
-                    tick_label_offset=parameters['tick_label_offset_2d'],
-                    plot_label=r'$\textbf{C}$',
-                    n_minor_ticks=parameters['n_minor_ticks_2d'],
-                    minor_tick_length=parameters['minor_tick_length_2d'],
-                    axis_label_angle=parameters['axis_label_angle_2d'],
-                    plot_label_offset=parameters['plot_label_offset_2d'])
+                    font_size=parameters['font_size'],
+                    tick_label_offset=parameters['tick_label_offset'],
+                    n_minor_ticks=parameters['n_minor_ticks'],
+                    minor_tick_length=parameters['minor_tick_length'],
+                    axis_label_angle=parameters['axis_label_angle'],
+                    plot_label_offset=parameters['plot_label_offset'])
 
 
-plot_snapshot(fig, parameters['azimuth'], parameters['altitude'])
+plot(fig)
 
 plt.savefig(figure_path + "_large.pdf")
 os.system(
