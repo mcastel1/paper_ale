84c1a692d7e11eba4453dc629d68cc7c2d8c5908
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-01-26 13:13:42 +0100

    adding polygon

--- figures/figure_10/plot.py
+++ figures/figure_10/plot.py
@@ -96,10 +96,11 @@ norm_v_min_max = cal.min_max_vector_field(snapshot_min,
 '''
 # I compute sigma_min_max from snapshots between snapshot_min + parameters['colorbar_sigma_snapshot_min_offset'] and snapshot_max. I do not use snapshot_min because there is a tension shock at the first few steps of the dynamics that would yield a huge negative value of sigma and an odd colorbars
 sigma_min_max = cal.min_max_files(
-                'def_sigma_n_12_', 
+                'def_sigma_n_12_',
                 os.path.join(solution_path + 'snapshots/csv/nodal_values'),
-                snapshot_min + parameters['colorbar_sigma_snapshot_min_offset'], 
-                snapshot_max, 
+                snapshot_min + \
+                    parameters['colorbar_sigma_snapshot_min_offset'],
+                snapshot_max,
                 parameters['frame_stride']
                  )
 '''
@@ -141,8 +142,10 @@ def plot_snapshot(fig, n_file, snapshot_label):
         solution_path + 'snapshots/csv/line_mesh_msh_n_' + n_snapshot + '.csv', usecols=columns_line_vertices)
     data_v = pd.read_csv(solution_path + 'snapshots/csv/nodal_values/def_v_n_' +
                          n_snapshot + '.csv', usecols=columns_v)
+
     data_u_msh_cur = pd.read_csv(
         solution_path + 'snapshots/csv/nodal_values/u_msh_n_' + n_snapshot + '.csv', usecols=columns_v)
+
     data_sigma = pd.read_csv(
         solution_path + 'snapshots/csv/nodal_values/def_sigma_n_12_' + n_snapshot + '.csv')
 
@@ -160,7 +163,31 @@ def plot_snapshot(fig, n_file, snapshot_label):
     fig.text(parameters['snapshot_label_position'][0], parameters['snapshot_label_position']
              [1], snapshot_label, fontsize=8, ha='center', va='center')
 
-    # gr.set_2d_axes_limits(ax, [0, 0], [parameters['L'], parameters['h']], [0, 0])
+    # 1) plot the polygon of the boundary 'ellipse_loop_id'
+    #
+    # build two a vector field which interpolates the displacement field in data_u_msh
+    U_interp_x, U_interp_y = vec.interpolating_function_2d_vector_field(
+        data_u_msh_cur)
+
+    # run through points in data_boundary_vertices_ellipse (reference configuration) and add to them [U_interp_x, U_interp_y] in order to obtain the boundary polygon in the current configuration
+    data_def_boundary_vertices_ellipse = []
+    for _, row in data_boundary_vertices_ellipse.iterrows():
+        data_def_boundary_vertices_ellipse.append(
+            np.add(
+                [row[':0'], row[':1']],
+                [U_interp_x(row[':0'], row[':1']),
+                 U_interp_y(row[':0'], row[':1'])]
+            )
+        )
+
+    # plot the boundary partial_omega_circle_out in the current configuration
+    partial_omega_circle_out_cur = Polygon(data_def_boundary_vertices_ellipse, fill=False,
+                                           linewidth=parameters['partial_omega_line_width'],
+                                           edgecolor='cyan',
+                                           linestyle='-.',
+                                           zorder=const.high_z_order)
+
+    ax.add_patch(partial_omega_circle_out_cur)
 
     # plot mesh for elastic problem and for mesh oustide the elastic body
     gr.plot_2d_mesh(ax, data_el_line_vertices,
@@ -168,7 +195,7 @@ def plot_snapshot(fig, n_file, snapshot_label):
     gr.plot_2d_mesh(ax, data_msh_line_vertices,
                     parameters['mesh_msh_line_width'], 'black', parameters['alpha_mesh'])
 
-    X, Y, V_x, V_y, grid_norm_v, norm_v_min, norm_v_max, _ = vec.interpolate_2d_vector_field(
+    X, Y, V_x, V_y, grid_norm_v, _, _, _ = vec.interpolate_2d_vector_field(
         data_v,
         [0, 0],
         [parameters['L'], parameters['h']],
@@ -178,7 +205,7 @@ def plot_snapshot(fig, n_file, snapshot_label):
     # fork
     # 1) to plot the figure, I set norm_v_min_max to the min and max for the current frame
     '''
-    norm_v_min_max= [norm_v_min, norm_v_max]     
+    norm_v_min_max= [norm_v_min, norm_v_max]
     '''
 
     _, _, U_msh_x, U_msh_y, _, _, _, _ = vec.interpolate_2d_vector_field(data_u_msh_cur,
@@ -195,10 +222,12 @@ def plot_snapshot(fig, n_file, snapshot_label):
     X_ref = np.array(lis.substract_lists_of_lists(X, U_msh_x))
     Y_ref = np.array(lis.substract_lists_of_lists(Y, U_msh_y))
     # 2. once the coordinates in the reference configuration are known, assess whether they fall within the elastic body by checking whether they fall wihin the ellipse
+    '''
     gr.set_inside_ellipse(
         X_ref, Y_ref, parameters['c'], parameters['a'], parameters['b'], 0, V_x, np.nan)
     gr.set_inside_ellipse(
         X_ref, Y_ref, parameters['c'], parameters['a'], parameters['b'], 0, V_y, np.nan)
+    '''
 
     # plot velocity of fluid
     vec.plot_2d_vector_field(ax, [X, Y], [V_x, V_y], parameters['shaft_length'], parameters['head_over_shaft_length'],
@@ -232,7 +261,7 @@ def plot_snapshot(fig, n_file, snapshot_label):
                     plot_label_font_size=parameters['panel_label_font_size'],                    n_minor_ticks=parameters['n_minor_ticks'],
                     minor_tick_length=parameters['minor_tick_length']
                     )
-
+    '''
     # =============
     # sigma subplot
     # =============
@@ -331,6 +360,8 @@ def plot_snapshot(fig, n_file, snapshot_label):
                     n_minor_ticks=parameters['n_minor_ticks'],
                     minor_tick_length=parameters['minor_tick_length']
                     )
+
+    '''
 
 
 plot_snapshot(fig, snapshot_max, rf'$t = \,$' + io.time_to_string(snapshot_max *
