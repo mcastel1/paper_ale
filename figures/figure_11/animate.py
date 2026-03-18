d112665b4a75d5d6f503d49545d35e196c3cc984
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-03-18 19:55:51 +0100

    arrow heads ok

--- figures/figure_11/animate.py
+++ figures/figure_11/animate.py
@@ -50,20 +50,19 @@ Writer = ani.writers['ffmpeg']
 writer = Writer(fps=plot.parameters['frames_per_second'], metadata=dict(
     artist='Michele'), bitrate=(int)(plot.parameters['bit_rate']))
 
-text.empty_texts(plot.fig)
+# text.empty_texts(plot.fig)
 
 
 def update_animation(n):
     print("Calling update_animation with n = ", n, " ... ", flush=True)
 
     # clear only the major axes of the plot. The colorbar axes need not be cleaned because make_colorbar already clears them
-    for ax in plot.fig.axes[:2]:
+    for ax in plot.fig.axes[:1]:
         ax.clear()
 
     # Clear text objects (the snapshot label accumulates)
     for txt in plot.fig.texts[:]:
         txt.remove()
-    # plot.gr.delete_all_axes(plot.fig)
 
     text.clear_labels_with_patterns(
         plot.fig, ["\second", "\msecond", "\minute", "\hour", "\pas"])
--- figures/figure_11/plot.py
+++ figures/figure_11/plot.py
@@ -57,6 +57,9 @@ plt.rcParams.update({
     )
 })
 
+
+
+
 print("Current working directory:", os.getcwd())
 print("Script location:", os.path.dirname(os.path.abspath(__file__)))
 
@@ -93,6 +96,9 @@ fig = pplt.figure(figsize=parameters['figure_size'], left=parameters['figure_mar
 fig.add_subplot(1, 1, 1)
 # fig.add_subplot(2, 1, 2)
 
+_colorbar_initialized = False
+
+
 v_colorbar_axis = fig.add_axes([parameters['v_colorbar_position'][0],
                                 parameters['v_colorbar_position'][1],
                                 parameters['v_colorbar_size'][0],
@@ -108,6 +114,9 @@ def plot_snapshot(fig, n_file,
                   snapshot_label='',
                   norm_v_min_max=None,
                   sigma_min_max=None):
+
+
+    global _colorbar_initialized
 
     start_time = time.time()
     last_time = start_time
@@ -174,22 +183,26 @@ def plot_snapshot(fig, n_file,
     print(f'4 took { time.time() - last_time}s')
     last_time =  time.time()
 
-    vp.plot_2d_vector_field(ax, [X, Y], [V_x, V_y], parameters['arrow_length'], parameters['head_over_shaft_length'], 30, 1, 1, 'red', 0,
+    vp.plot_2d_vector_field(ax, [X, Y], [V_x, V_y], parameters['arrow_length'], parameters['head_over_shaft_length'], 30, 1, 1, 'color_from_map', 0,
                             clip_on=False)
     
     print(f'5 took { time.time() - last_time}s')
     last_time =  time.time()
 
-    # gr.cb.make_colorbar(fig, grid_norm_v, norm_v_min_max[0], norm_v_min_max[1], parameters['v_colorbar_position'], parameters['v_colorbar_size'],
-    #                     label=parameters['v_colorbar_axis_label'],
-    #                     font_size=parameters['v_colorbar_font_size'],
-    #                     tick_length=parameters['v_colorbar_tick_length'],
-    #                     label_pad=parameters['v_colorbar_label_offset'],
-    #                     tick_label_offset=parameters['v_colorbar_tick_label_offset'],
-    #                     tick_label_angle=parameters['v_colorbar_tick_label_angle'],
-    #                     line_width=parameters['v_colorbar_line_width'],
-    #                     tick_label_format=parameters['v_colorbar_tick_label_format'],
-    #                     axis=v_colorbar_axis)
+    if not _colorbar_initialized:
+
+        gr.cb.make_colorbar(fig, grid_norm_v, norm_v_min_max[0], norm_v_min_max[1], parameters['v_colorbar_position'], parameters['v_colorbar_size'],
+                            label=parameters['v_colorbar_axis_label'],
+                            font_size=parameters['v_colorbar_font_size'],
+                            tick_length=parameters['v_colorbar_tick_length'],
+                            label_pad=parameters['v_colorbar_label_offset'],
+                            tick_label_offset=parameters['v_colorbar_tick_label_offset'],
+                            tick_label_angle=parameters['v_colorbar_tick_label_angle'],
+                            line_width=parameters['v_colorbar_line_width'],
+                            tick_label_format=parameters['v_colorbar_tick_label_format'],
+                            axis=v_colorbar_axis)
+        
+        _colorbar_initialized = True
 
     print(f'6 took { time.time() - last_time}s')
     last_time =  time.time()
