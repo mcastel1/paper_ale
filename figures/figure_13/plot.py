0e717de2b5a888193e22ab7381eaf9dcb2520758
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2026-05-29 16:23:50 +0200

    saved data for figure_13

--- figures/figure_13/plot.py
+++ figures/figure_13/plot.py
@@ -85,7 +85,7 @@ figure_path = os.path.join(os.path.dirname( os.path.abspath(__file__)), paramete
 snapshot_min, snapshot_max = sys_utils.n_min_max(
     'line_mesh_n_', snapshot_path)
 #cut the animation at a custom snapshot
-# snapshot_max = parameters['custom_snapshot_max']
+snapshot_max = parameters['custom_snapshot_max']
 
 number_of_frames = snapshot_max - snapshot_min + 1
 
